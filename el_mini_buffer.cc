8b8fdb806076141863d5198b19f39adac15a7368
ASMlover <asmlover@126.com>
2014-12-08 23:35:17 +0800

    implement the MiniBuffer

--- el_mini_buffer.cc
+++ el_mini_buffer.cc
@@ -32,36 +32,83 @@ namespace el {
 MiniBuffer::MiniBuffer(void)
   : buffer_(nullptr)
   , length_(0)
-  , storage_(0) {
+  , storage_(DEF_STORAGE) {
 }
 
 MiniBuffer::~MiniBuffer(void) {
+  Destroy();
 }
 
 bool MiniBuffer::Init(int storage) {
+  storage_ = (storage > DEF_STORAGE ? storage : DEF_STORAGE);
+  length_ = 0;
+  buffer_ = (char*)malloc(storage_);
+  if (nullptr == buffer_)
+    return false;
+
   return true;
 }
 
 void MiniBuffer::Destroy(void) {
+  if (nullptr != buffer_) {
+    free(buffer_);
+    buffer_ = nullptr;
+  }
+  length_ = 0;
+  storage_ = DEF_STORAGE;
 }
 
 int MiniBuffer::Put(const char* buffer, int bytes) {
-  return 0;
+  if (length_ + bytes > storage_) {
+    if (!Regrow())
+      return 0;
+  }
+
+  memcpy(buffer_ + length_, buffer, bytes);
+  length_ += bytes;
+
+  return bytes;
 }
 
 int MiniBuffer::Get(int bytes, char* buffer) {
-  return 0;
+  int copy_bytes = length_ <= bytes ? length_ : bytes;
+  if (0 == copy_bytes)
+    return 0;
+
+  memcpy(buffer, buffer_, copy_bytes);
+  if (copy_bytes != length_)
+    memmove(buffer_, buffer_ + copy_bytes, length_ - copy_bytes);
+  length_ -= copy_bytes;
+
+  return copy_bytes;
 }
 
 int MiniBuffer::IncrementLength(int bytes) {
-  return 0;
+  if (length_ + bytes > storage_) {
+    if (!Regrow())
+      return 0;
+  }
+
+  length_ += bytes;
+  return bytes;
 }
 
 int MiniBuffer::DecrementLength(int bytes) {
-  return 0;
+  EL_ASSERTX(length_ >= bytes, "buffer length must >= bytes");
+
+  if (bytes < length_)
+    memmove(buffer_, buffer_ + bytes, length_ - bytes);
+  length_ -= bytes;
+
+  return bytes;
 }
 
 bool MiniBuffer::Regrow(void) {
+  int new_storage = (0 != storage_ ? 2 * storage_ : DEF_STORAGE);
+  buffer_ = (char*)realloc(buffer_, new_storage);
+  EL_ASSERTX(nullptr != buffer_, "realloc buffer failed");
+
+  storage_ = new_storage;
   return true;
 }
 

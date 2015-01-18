760c84c7aa73d1933af52a623053675d973a2b1c
ASMlover <asmlover@126.com>
2015-01-18 19:18:20 +0800

    updated the building script for windows platform

--- Makefile.mk
+++ Makefile.mk
@@ -25,25 +25,29 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-OUT	= eutil.exe 
+OUT_LIB	= libeutil.lib
+OUT_BIN	= eutil.exe
+OUT	= $(OUT_LIB) $(OUT_BIN)
 RM	= del 
 CC	= cl -c -nologo
+AR	= lib -nologo
 MT	= mt -nologo
 LINK	= link -nologo
 CFLAGS	= -O2 -W3 -MDd -GS -Zi -Fd"vc.pdb" -EHsc -D_DEBUG\
 	-D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS -wd4290
-LDFLAGS	= -INCREMENTAL -DEBUG -PDB:$(OUT).pdb -manifest\
-	-manifestfile:$(OUT).manifest -manifestuac:no winmm.lib 
-OBJS	= el_main.obj el_locker_test.obj el_condition_test.obj\
-	el_singleton_test.obj el_object_pool_test.obj el_object_mgr_test.obj\
-	el_thread_test.obj el_thread_pool_test.obj el_logging_test.obj\
-	el_time_test.obj el_symbol_table_test.obj el_io_test.obj\
-	el_ini_parser_test.obj\
-	\
-	el_win_io.obj el_win_condition.obj\
+LDFLAGS	= -INCREMENTAL -DEBUG -PDB:$(OUT_BIN).pdb -manifest\
+	-manifestfile:$(OUT_BIN).manifest -manifestuac:no\
+	winmm.lib libeutil.lib
+OBJS_LIB= el_win_io.obj el_win_condition.obj\
 	\
 	el_io.obj el_time.obj el_thread_pool.obj el_logging.obj el_unit.obj\
 	el_symbol_table.obj el_ini_parser.obj el_mini_buffer.obj
+OBJS_BIN= el_main.obj el_locker_test.obj el_condition_test.obj\
+	el_singleton_test.obj el_object_pool_test.obj el_object_mgr_test.obj\
+	el_thread_test.obj el_thread_pool_test.obj el_logging_test.obj\
+	el_time_test.obj el_symbol_table_test.obj el_io_test.obj\
+	el_ini_parser_test.obj
+OBJS	= $(OBJS_LIB) $(OBJS_BIN)
 
 
 
@@ -60,9 +64,12 @@ clean:
 
 
 
-$(OUT): $(OBJS)
-	$(LINK) -out:$(OUT) $(OBJS) $(LDFLAGS)
-	$(MT) -manifest $(OUT).manifest -outputresource:$(OUT);1
+$(OUT_LIB): $(OBJS_LIB)
+	$(AR) -out:$(OUT_LIB) $(OBJS_LIB)
+
+$(OUT_BIN): $(OBJS_BIN)
+	$(LINK) -out:$(OUT_BIN) $(OBJS_BIN) $(LDFLAGS)
+	$(MT) -manifest $(OUT_BIN).manifest -outputresource:$(OUT_BIN);1
 
 .cc.obj:
 	$(CC) $(CFLAGS) $<
